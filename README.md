1c8e11f9f502497131a7851df0643ada8b9f711e
Rahul <rahulbobyrb6@gmail.com>
2026-05-30 19:44:48 +0530

    Final Commit after v3

--- README.md
+++ README.md
@@ -18,21 +18,21 @@ The automobile_classifier_v1 will focus on KIA models seltos and carnival only
 - Unfroze the layer4 with the model.fc
 
 First run: 
-![Confusion Matrix](image.png)
+![Confusion Matrix](utils\image.png)
 - Carnival dataset had many U.S.A. variants of the carnival which looks very different from the Indian
 - image size was set 
 
 Second run:
-![Confusion Matrix](image-1.png)
+![Confusion Matrix](utils\image-1.png)
 - Seltos dataset had noise -> CLEARED
 
 Third run: 
-![Confusion Matrix](image-2.png)
+![Confusion Matrix](utils\image-2.png)
 - Image size was changed to 384, and batch size to 32
 - 20 epochs trained
 
 Final Run:
-![Confusion Matrix](image-3.png)
+![Confusion Matrix](utils\image-3.png)
 - Increased the image size to 500, batch size to 64
 - 20 epochs
 - Dataset was highly cleaned - 400 to 500 images per class
@@ -43,3 +43,21 @@ Final Run:
 
 # Models:
 The models for each version are given in this drive link: [Automobile Classifier Models](https://drive.google.com/drive/folders/1Dzo2N9qPnRtoXx5FNe19c2tCIPrvAa0Y?usp=sharing)
+
+# v3
+- Added Kia EV6
+- Increaed dataset from ~400 images to ~900 iamges for each class 
+- total ~3600 images combined
+- Increased number of epochs from 25 to 40
+- Decreased learning rate from 1e-5 to 4e-6
+
+![Confusion Matrix](utils\image-4.png)
+![Loss and Accuracy Curve while training](utils\image-5.png)
+- Time taken to train: 73mins - but I forced stop at 39th epoch as it started mild overfitting
+``` 
+--------------------------------------------------
+Epoch [38/40]
+Train Loss: 0.0358 | Train Accuracy: 99.27%
+Val Loss:   0.0997 | Val Accuracy:   96.94%
+--------------------------------------------------
+```
---
+++ automobile_classifier_v3/app.py
@@ -0,0 +1,159 @@
+import streamlit as st
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+from PIL import Image
+from inference import load_model, predict, transform_image
+from configs import DEVICE, MODEL_PATH
+import numpy as np
+from gradcam import generate_gradcam
+
+# -----------------------------
+# CONFIG
+# -----------------------------
+
+# -----------------------------
+# PAGE CONFIG
+# -----------------------------
+st.set_page_config(
+    page_title="KIA Model Classifier",
+    layout="centered"
+)
+
+# -----------------------------
+# MODEL LOADING
+# -----------------------------
+model, CLASS_NAMES, NUM_CLASSES, IMAGE_SIZE, ARCHITECTURE = load_model(MODEL_PATH)
+# print("DEBUG: Model loaded successfully.")
+
+# -----------------------------
+# TRANSFORM
+# -----------------------------
+transform = lambda image: transform_image(image, IMAGE_SIZE)
+# print("DEBUG: Transform function defined successfully.")
+
+# -----------------------------
+# SESSION STATE
+# -----------------------------
+if "page" not in st.session_state:
+    st.session_state.page = "main"
+
+# -----------------------------
+# PAGE FUNCTIONS
+# -----------------------------
+def go_main():
+    st.session_state.page = "main"
+
+
+def go_details():
+    st.session_state.page = "details"
+
+# -----------------------------
+# MAIN PAGE
+# -----------------------------
+if st.session_state.page == "main":
+
+    st.title("KIA Model Classifier")
+    st.write("Upload an image for classification.")
+
+    uploaded_file = st.file_uploader(
+        "Upload Image",
+        type=["jpg", "jpeg", "png"]
+    )
+
+    if uploaded_file is not None:
+
+        image = Image.open(uploaded_file).convert("RGB")
+
+        st.image(
+            image,
+            caption="Uploaded Image",
+            use_container_width=True
+        )
+
+        predicted_class, confidence_score, image_tensor, predicted_index = predict(
+            image=image,
+            model=model,
+            transform=transform,
+            class_names=CLASS_NAMES
+        )
+        print("DEBUG: Prediction completed successfully.")
+
+        original_image = np.array(image).astype(np.float32) / 255.0
+
+        with torch.enable_grad():
+            gradcam_image = generate_gradcam(
+                model=model,
+                image_tensor=image_tensor,
+                original_image=original_image,
+                target_class=predicted_index
+            )
+
+        st.success(f"Prediction: {predicted_class.upper()}")
+        st.info(f"Confidence: {confidence_score:.2f}%")
+
+        st.subheader("Grad-CAM Visualization")
+        st.image(
+            gradcam_image,
+            caption="Grad-CAM (Model Attention) Heatmap",
+            use_container_width=True
+        )
+
+    st.divider()
+
+    if st.button("View Model Details"):
+        go_details()
+        st.rerun()
+    # print("DEBUG: MODEL details rendered successfully.")
+
+# -----------------------------
+# DETAILS PAGE
+# -----------------------------
+elif st.session_state.page == "details":
+
+    st.title("Model Details")
+
+    st.subheader("Architecture")
+    st.write(ARCHITECTURE)
+
+    st.subheader("Classifier Head")
+    st.code(f"Linear(2048 → {NUM_CLASSES})")
+
+    st.subheader("Detected Classes")
+    st.write(CLASS_NAMES)
+
+    st.subheader("Number of Classes")
+    st.write(NUM_CLASSES)
+
+    st.subheader("Input Specifications")
+    st.write(f"Image Size: {IMAGE_SIZE} × {IMAGE_SIZE}")
+    st.write("Input Type: RGB Image")
+
+    st.subheader("Preprocessing")
+    st.code("""
+        Resize()
+        ToTensor()
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    """)
+
+    st.subheader("Training Setup")
+    st.write("Transfer Learning")
+    st.write("Pretrained ImageNet ResNet50 Backbone")
+    st.write("Custom Final Classification Layer")
+    st.write("Loss Function: CrossEntropyLoss")
+    st.write("Optimizer: Adam")
+
+    st.subheader("Model File")
+    st.code(MODEL_PATH)
+
+    st.subheader("Inference Device")
+    st.write(str(DEVICE).upper())
+
+    st.divider()
+
+    if st.button("Back to Classifier"):
+        go_main()
+        st.rerun()
